79470a0ab832c4a422d1127416b1a09f8db9ebf8
hisheng <Rieman@Riemans-MacBook-Pro.local>
2016-06-17 18:39:45 +0800

    new

---
+++ Assets/SolidController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SolidController : MonoBehaviour {
+	//public Transform ca;
+
+	// Use this for initialization
+	void Start () {
+
+		 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.CompareTag("star")){
+			Destroy(other.gameObject);
+		}
+		if(other.CompareTag("bluestar")){
+			Destroy(other.gameObject);
+		}
+	}
+}
