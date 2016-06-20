a40d6b3ec195a2e91e5e9e5f9363bec728201324
hisheng <Rieman@macbookpro-2d30.ztgame.com>
2016-06-20 18:16:36 +0800

    增加多个场景
    增加多个场景

---
+++ Assets/second/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
+public class LevelManager : MonoBehaviour {
+
+
+
+
+	public	void LoadScene(string name){
+		//Application.LoadLevel (name);
+		SceneManager.LoadScene(name);
+	}
+
+
+}
