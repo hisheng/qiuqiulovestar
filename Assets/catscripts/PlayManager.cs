484707516e129e72f078e5e40d61c67eb8c94075
hisheng <Rieman@macbookpro-2d30.ztgame.com>
2016-06-21 17:45:21 +0800

    增加动画类
    增加动画类

---
+++ Assets/catscripts/PlayManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayManager : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W)){
+			anim.SetInteger ("state", 1);
+		}
+
+		if(Input.GetKey(KeyCode.R)){
+			anim.SetInteger ("state", 2);
+		}
+
+	}
+}
