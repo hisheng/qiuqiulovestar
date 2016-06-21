ba8f88e22b64a093af2d2c4f6c4546a9c94ced52
hisheng <Rieman@Riemans-MacBook-Pro.local>
2016-06-21 10:17:18 +0800

    增加多个menu
    方法1 场景切换
    方法2  通过gameobject 的setactive

--- Assets/second/LevelManager.cs
+++ Assets/second/LevelManager.cs
@@ -5,12 +5,27 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 
+	public Transform mainMenu, optionMenus;
 
 
 
 	public	void LoadScene(string name){
 		//Application.LoadLevel (name);
 		SceneManager.LoadScene(name);
+	}
+
+	public void QuitGame(){
+		Application.Quit ();
+	}
+
+	public void OptionMenu(bool clicked){
+		if (clicked) {
+			optionMenus.gameObject.SetActive (clicked);	
+			mainMenu.gameObject.SetActive (false);
+		} else {
+			optionMenus.gameObject.SetActive (clicked);	
+			mainMenu.gameObject.SetActive (true);
+		}
 	}
 
 
