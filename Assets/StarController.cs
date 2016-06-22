2766fe88a938d27ab3b1b158a39e2109a3ebe111
hisheng <Rieman@macbookpro-2d30.ztgame.com>
2016-06-22 16:05:39 +0800

    适配修改1
    适配修改1

--- Assets/StarController.cs
+++ Assets/StarController.cs
@@ -2,15 +2,17 @@
 using System.Collections;
 
 public class StarController : MonoBehaviour {
-
+//	public Transform panel;
 	public GameObject[] stars;
 	public float shoottime;
 
+	private float width;
 
 	// Use this for initialization
 	void Start () {
 		shoottime = 0;
-		 
+		//width = Screen.width;
+		//print (width);
 			
 	}
 	
@@ -18,7 +20,10 @@ public class StarController : MonoBehaviour {
 	void Update () {
 		shoottime = shoottime + Time.deltaTime;
 		if(shoottime > 1){
-			Instantiate(stars[Random.Range(0,4)], new Vector3(Random.value*10.0f-5.0f, 4, 0), Quaternion.identity);
+			GameObject obj = Instantiate(stars[Random.Range(0,4)], new Vector3(Random.value*10.0f-5.0f, 4, 0), Quaternion.identity) as GameObject;
+//			GameObject obj = Instantiate(stars[Random.Range(0,4)]) as GameObject;
+//			obj.transform.SetParent (panel);
+//			obj.transform.localPosition = new Vector3 (Random.Range (-Screen.width / 2, Screen.width / 2), 0, 0);
 			shoottime = 0;
 		}
 	}
